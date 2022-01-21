1270e778306008b8b2909ce8d157d1c8ea7e42c3
boocipher <boocipher@gmail.com>
2022-01-20 23:58:56 -0600

    f for div color

--- assets/script.js
+++ assets/script.js
@@ -8,5 +8,25 @@ function showDate() {
 showDate();
 
 // function for div color based on current time
+
+// iterates over all input elements to set their background color
+$(document).ready(function() {
+    var currentHour = moment().format('H');
+    var parsedHour = parseInt(currentHour, 10);
+    console.log(parsedHour);
+    $("input").each(function(index, element) {
+        $(element).css("backgroundColor", "gray");
+        var attr = $(element).attr("id");
+        console.log(attr);
+        var parsedAttr = parseInt(attr, 10);
+        console.log(parsedAttr);
+        if (parsedHour === parsedAttr) {
+            $(element).css("backgroundColor", "red");
+        } else if (parsedHour < parsedAttr) {
+            $(element).css("backgroundColor", "green");
+        }
+    });
+});
+
 // save with local storage
 // pull saved events when adding new

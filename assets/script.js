5eea4f25c15db31ada75ce1b3785e1de1d083195
boocipher <boocipher@gmail.com>
2022-01-23 15:49:44 -0600

    save to local storage and evnt listener

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
+showDate();
+setColor();
+setInterval(setColor, 60000); // CHECK time and update div color without refresh every minute
+
+
 // show current time&date in header with moment.js
 function showDate() {
     var setDate = moment().format('MMMM Do YYYY, h:mm:ss a');
@@ -5,37 +10,43 @@ function showDate() {
     $('#currentDay').text(setDate);
 }
 
-showDate();
-
 // function for div color based on current time
-
 // iterates over all input elements to set their background color
+function setColor() {
 $(document).ready(function() {
     var currentHour = moment().format('H');
     var parsedHour = parseInt(currentHour, 10);
-    console.log(parsedHour);
+    console.log('Current hour string to int (parsed) is: ' + parsedHour);
     $("input").each(function(index, element) {
         $(element).css("backgroundColor", "gray");
         var attr = $(element).attr("id");
-        console.log(attr);
+        console.log('Field id is: ' + attr);
         var parsedAttr = parseInt(attr, 10);
-        console.log(parsedAttr);
+        console.log('Id to int (parsed): ' + parsedAttr);
         if (parsedHour === parsedAttr) {
             $(element).css("backgroundColor", "red");
         } else if (parsedHour < parsedAttr) {
-            $(element).css("backgroundColor", "green");
+            $(element).css("backgroundColor", "lightgreen");
         }
-    // generate key and get item from local storage
-    var key = moment().format('YYYY-MM-DD');
-    key = key + "-" + attr;
-    console.log(key);
+       
     });
+
 });
+}
 
 // save with local storage
-// listener function for the button
-// from listener/button event, which hour is clicked
-// get text from the input element
-// setItem 
+function handleSave(event) {
+    // get the id of saveBtn that is clicked
+    var hourId = $(this).attr("data-index");
+    console.log('button clicked id: ' + hourId);
+    // save data from the input area in local storage
+    var key = moment().format('YYYY-MM-DD');
+    key = key + "-" + hourId;
+    localStorage.setItem(key, $("input#" + hourId).val());
+    console.log('The value for Set localStorage is: ', $("input#" + hourId).val());
+  }
 
-// pull saved events when adding new
+// listener function for the button; attaches handler for the save btns
+$(".saveBtn").on("click", handleSave);
+
+
